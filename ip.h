509ae20608d57dcc0bc1f088f266bc56b014f7b0
satoken <satoken@moguera.dna.bio.keio.ac.jp>
2013-12-24 17:43:09 +0900

    implement IP model, and add ip.cpp and ip.h.

---
+++ ip.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2012 Kengo Sato
+ *
+ * This file is part of DAFS.
+ *
+ * DAFS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DAFS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DAFS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __INC_IP_H__
+#define __INC_IP_H__
+
+class IPimpl;
+
+class IP
+{
+public:
+  typedef enum {MIN, MAX} DirType;
+  typedef enum {FR, LO, UP, DB, FX} BoundType;
+
+public:
+  IP(DirType dir, int n_th);
+  ~IP();
+  int make_variable(double coef);
+  int make_constraint(BoundType bnd, double l, double u);
+  void add_constraint(int row, int col, double val);
+  void update();
+  double solve();
+  double get_value(int col) const;
+
+private:
+  IPimpl* impl_;
+};
+
+#endif  // __INC_IP_H__
+
+// Local Variables:
+// mode: C++
+// End:
